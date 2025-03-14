20517e78089f7ec5f00aac6c021da310f43b115d
chuck765 <765g.kotetsu@gmail.com>
2025-03-15 08:46:31 +0900

    update: read_data.py

--- pandas/modules/read_data.py
+++ pandas/modules/read_data.py
@@ -11,9 +11,14 @@ class Order:
 def get_order(order_file: str) -> List[Order]:
     order_df = pd.read_csv(order_file)
     order_df.columns = ['order_no1', 'order_no2']
-    order_df['order_no2'] = order_df['order_no1'] # 値の差し替え（列単位）
-    print(order_df)
+    order_df['order_no2'] = order_df['order_no1'] # 値の差し替え（列単位）    
+    orders = [
+        Order(id=idx, order_no=row['order_no2']) for idx, row in order_df.iterrows()
+    ]
+    
+    return orders
 
 
 if __name__ == "__main__":
-    get_order('../input/order.csv')
+    orders = get_order('../input/order.csv')
+    print(orders)
